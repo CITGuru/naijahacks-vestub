420693fa652e07482229ad933354969616ab132d
Oyetoke Toby <oyetoketoby80@gmail.com>
2018-10-24 14:00:31 +0100

    Worked on USSD

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.post('/ussd', (req, res) => {
         var text = text.trim()
         // console.log('yes')
         if (text != '0') {
-            if (!phone.match(/\+?[0-9]{5,15}/igm)) {
+            if (!text.match(/\+?[0-9]{5,15}/igm)) {
                 var phone = text
                 phoneSessionObject[sessionNumber].action = 'phone'
                 res.status(401).send({
